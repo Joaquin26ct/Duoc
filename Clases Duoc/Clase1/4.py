b34720877c23ee878993aa61dad72d5e192048c7
Joaquin26ct <130670114+Joaquin26ct@users.noreply.github.com>
2023-04-14 11:11:09 -0400

    Update 4.py

--- Clases Duoc/Clase1/4.py
+++ Clases Duoc/Clase1/4.py
@@ -1,5 +1,3 @@
-# create a system that allows to validate the user and password from a worker, the only two users are pedro and angel, and their passwords are 1234 and a4s1
-
 def validate_user(user, password):
     if user == "pedro" and password == "1234":
         return True
